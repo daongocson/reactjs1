4757219bb196e6531ce9d29e9eae380dcd842a97
SON DAO <ngocsonit@gmai.com>
2024-10-17 17:19:07 +0700

    add view error

--- src/pages/listeeror.jsx
+++ src/pages/listeeror.jsx
@@ -1,12 +1,16 @@
 import { Button, Input, Modal, notification, Table } from "antd";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 import { getLsErrorApi } from "../util/api";
-import { CaretRightOutlined, DeleteOutlined, EyeOutlined, UsergroupAddOutlined } from "@ant-design/icons";
+import { EyeOutlined } from "@ant-design/icons";
+import Draggable from 'react-draggable';
+
 const { Search } = Input;
 
 const ListErorPage = () => {
     const [dataSource, setDataSource] = useState([]);
+    const [disabled, setDisabled] = useState(true);
+
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [modaldata, setmodaldata] = useState([]);
     const keys  = ["TenBS","ndloi","MaKhoa","MaVP","TenDV"];    
@@ -26,7 +30,13 @@ const ListErorPage = () => {
         fetchUser();        
     }, [])
     const onSearch = (value, _e, info) => console.log(info?.source, value);
-
+    const [bounds, setBounds] = useState({
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+      });
+      const draggleRef = useRef(null);
     const columns = [
         {
             title: 'Mã VP',
@@ -75,6 +85,19 @@ const ListErorPage = () => {
                 keys.some((key)=>item[key].toLowerCase().includes(keyword.toLowerCase())
             )));        
     }
+    const onStart = (_event, uiData) => {
+        const { clientWidth, clientHeight } = window.document.documentElement;
+        const targetRect = draggleRef.current?.getBoundingClientRect();
+        if (!targetRect) {
+          return;
+        }
+        setBounds({
+          left: -targetRect.left + uiData.x,
+          right: clientWidth - (targetRect.right - uiData.x),
+          top: -targetRect.top + uiData.y,
+          bottom: clientHeight - (targetRect.bottom - uiData.y),
+        });
+    }
     return (       
         <div style={{ padding: 30 }}>      
             <div className="ant-col ant-col-xs-24 ant-col-xl-8">             
@@ -104,15 +127,48 @@ const ListErorPage = () => {
           
             />
               <Modal
-                title="Chi tiết bệnh nhân"
+                 title={
+                    <div
+                      style={{
+                        width: '100%',
+                        cursor: 'move',
+                      }}
+                      onMouseOver={() => {
+                        if (disabled) {
+                          setDisabled(false);
+                        }
+                      }}
+                      onMouseOut={() => {
+                        setDisabled(true);
+                      }}
+                      // fix eslintjsx-a11y/mouse-events-have-key-events
+                      // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/mouse-events-have-key-events.md
+                      onFocus={() => {}}
+                      onBlur={() => {}}
+                      // end
+                    >
+                      Draggable Modal
+                    </div>
+                  }
                 open={isModalVisible}
                 onOk={handleOk}
                 onCancel={handleCancel}
+                modalRender={(modal) => (
+                    <Draggable
+                      disabled={disabled}
+                      bounds={bounds}
+                      nodeRef={draggleRef}
+                      onStart={(event, uiData) => onStart(event, uiData)}
+                    >
+                      <div ref={draggleRef}>{modal}</div>
+                    </Draggable>
+                  )}
             >
                 <p>Mã VP: {modaldata.MaVP}</p>
                 <p>Mã BN: {modaldata.MaBN}</p>                
                 <p>{modaldata.TenBS}({modaldata.MaBS})</p>
                 <p>{modaldata.TenDV}</p>
+                <b>Nội dung lỗi:</b>
                 <p>{modaldata.ndloi}</p>
             </Modal>
             <>Total {dataSource.length} items</>
