213d50e8be347d5de5ef8e1452e0871ce128520c
daongocson <ngocsonit@gmail.com>
2025-02-26 15:46:39 +0700

    addbc

--- src/pages/tracuuicd.jsx
+++ src/pages/tracuuicd.jsx
@@ -1,4 +1,4 @@
-import { AutoComplete, Button, DatePicker, Input, notification, Select, Space, Table } from "antd";
+import { AutoComplete, Button, DatePicker, Input, notification, Select, Space, Table, Tabs } from "antd";
 import { useState } from "react";
 import { postbaocaoIcdApi, postDoctorApi, postIcdApi } from "../util/api";
 import { SearchOutlined } from "@ant-design/icons";
@@ -11,8 +11,10 @@ const TracuuICDPage = () => {
     const [dateOp, setDateOp] = useState([]); 
     const [dataBaocao, setDataBaocao] = useState([]); 
     const [icd, setICD] = useState(''); 
+    const [dataCaptoa, setDataCaptoa]= useState([]); 
+    const [dataChuyentuyen, setDataChuyentuyen]= useState([]); 
 
-    const columnbs = [
+    const columns = [
         {
             title: 'Mã VP',
             dataIndex: 'patientrecordid',
@@ -29,6 +31,7 @@ const TracuuICDPage = () => {
         }      
 
     ];  
+    const keys  = ["loairv","patientrecordid"]
     const handleOnSearch = async(values) => {   
         if(values.length==2)  {
             const res = await postIcdApi(values);
@@ -73,7 +76,7 @@ const TracuuICDPage = () => {
                         description: res.thongbao
                     })
                 }else
-                    setDataBaocao(res);   
+                setFilData(res);
             } else {
                 notification.error({
                     message: "Unauthorized",
@@ -82,11 +85,25 @@ const TracuuICDPage = () => {
             }
         
         
-      };
+      };    
+    const setFilData=(data)=>{
+        setDataBaocao(data);
+        // console.log("sestffff>>>>",data);
+        setDataCaptoa(data.filter(item=> item.dm_hinhthucravienid===2));       
+        console.log("datacccsestffff>>>>",dataCaptoa); 
+        setDataChuyentuyen(data.filter(item=> item.dm_hinhthucravienid===13));
+    };  
       const onChangeDate = (date, dateString) => {        
         // console.log("date", dateString);
         setDateOp(dateString);
       };
+      const searchTable=(data)=>{   
+        return data;
+        // return data.filter(
+        //     (item)=>(
+        //         keys.some((key)=>item[key].toLowerCase().includes(keyword.toLowerCase())
+        //     )));      
+    }
     return (
         <>         
           <Space.Compact key={"spacehs"} block>
@@ -110,35 +127,79 @@ const TracuuICDPage = () => {
             </Space.Compact>     
            
             <Space wrap direction="horizontal" size={2}>                                       
-                    <Select
+                    {/* <Select
                             defaultValue="Chưa sử dụng"
                             style={{ width: 220 }}
-                        //    onChange={handleChange}
-                            // options={[
-                            //     { value: 'Khoa nội', label: 'Khoa nội' },
-                            //     { value: 'Khoa khám bệnh', label: 'Khoa khám bệnh' },
-                            //     { value: 'Khoa ngoại', label: 'Khoa ngoại' },
-                            //     { value: 'Khoa YHCT', label: 'Khoa YHCT' },
-                            //     { value: 'Khoa Sản', label: 'Khoa Sản' },
-                            // ]}
-                    />  
-                     <Select
+                           onChange={handleChange}
+                            options={[
+                                { value: 'Khoa nội', label: 'Khoa nội' },
+                                { value: 'Khoa khám bệnh', label: 'Khoa khám bệnh' },
+                                { value: 'Khoa ngoại', label: 'Khoa ngoại' },
+                                { value: 'Khoa YHCT', label: 'Khoa YHCT' },
+                                { value: 'Khoa Sản', label: 'Khoa Sản' },
+                            ]}
+                    />   */}
+                     {/* <Select
                             defaultValue="Hình thức ra viện"
                             style={{ width: 220 }}
-                        //    onChange={handleChange}
-                            // options={[
-                            //     { value: 'Nhập viện', label: 'Nhập viện' },
-                            //     { value: 'Chuyển viện', label: 'chuyển viện' },
-                            //     { value: 'Điều trị ngoại trú', label: 'Điều trị ngoại trú' }                                
-                            // ]}
-                    />                                    
-                </Space>
-                <p><strong>Số lượng: {dataBaocao.length}</strong></p>
-                <Table
-                    rowKey={"medicalrecordid"}
-                    bordered
-                    dataSource={dataBaocao} columns={columnbs}                                              
-                />   ,
+                           onChange={handleChange}
+                            options={[
+                                { value: 'Nhập viện', label: 'Nhập viện' },
+                                { value: 'Chuyển viện', label: 'chuyển viện' },
+                                { value: 'Điều trị ngoại trú', label: 'Điều trị ngoại trú' }                                
+                            ]}
+                    />                                     */}
+                </Space>                
+                <Tabs
+                    defaultActiveKey="1"
+                    items={[
+                    {
+                        label: `BN Chuyển tuyến (${dataChuyentuyen.length})`,
+                        key: 'b1',
+                        children: [ 
+                            <Search
+                            placeholder="Nhập nội dung"
+                            key={"seachCxl"}
+                            allowClear
+                            onChange={(event)=>setKeyword(event.target.value)}                      
+                            style={{
+                                width: "30%"                        
+                            }}
+                            /> ,
+                            <Table   
+                            rowKey={"medicalrecordid"}                    
+                            bordered                       
+                            dataSource={searchTable(dataChuyentuyen)} columns={columns}                 
+                            key="cskhkhl"
+                            /> ,
+                            'Số lượng: '+ dataChuyentuyen.length                     
+                        ],
+                    },{
+                        label: `Cấp toa cho về (${dataCaptoa.length})`,
+                        key: 'bn2',
+                        children: [ 
+                            <Table   
+                            rowKey={"medicalrecordid"}                    
+                            bordered
+                            dataSource={dataCaptoa} columns={columns}                       
+                            key="cskhk1hl"
+                            /> ,
+                            'Số lượng: '+ dataCaptoa.length                     
+                        ],
+                    },{
+                        label: `Tất cả BN(${dataBaocao.length})`,
+                        key: 'bn3',
+                        children: [ 
+                            <Table
+                                rowKey={"medicalrecordid"}
+                                bordered
+                                dataSource={dataBaocao} columns={columns}                                              
+                            />  ,
+                            'Số lượng: '+ dataBaocao.length                     
+                        ],
+                    }
+                    ]}
+                />                 
         </>
     )
 }
