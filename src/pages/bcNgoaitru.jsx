c5f5c9549803c98cb0b32c2873dbcc135cc313cf
daongocson <ngocsonit@gmail.com>
2025-03-03 14:38:58 +0700

    add pk

--- src/pages/bcNgoaitru.jsx
+++ src/pages/bcNgoaitru.jsx
@@ -9,6 +9,8 @@ const BCNgoaitruPage = () => {
     const [dateOp, setDateOp] = useState([]);   
     const [datacc, setDatacc]= useState([]); 
     const [dataKham, setdataKham]= useState([]);    
+    const [dataKhamxtri, setdataKhamxtri]= useState([]);    
+    const [dataKhamkkb, setdataKhamkkb]= useState([]);    
     const [dataChuyentuyen, setdataChuyentuyen]= useState([]);   
     const columns_nt = [
         {
@@ -17,7 +19,7 @@ const BCNgoaitruPage = () => {
         },
         {
             title: 'Thời gian',
-            dataIndex: 'ngayrv',
+            dataIndex: 'tgkham',
         }
     ]; 
     const OnClickHs = async () => {      
@@ -46,21 +48,29 @@ const BCNgoaitruPage = () => {
             }
         
         
-      };    
+      }; 
+    const changeKhoaKB=(a,b)=>{
+        console.log("dfadfadf>",a,b);
+        setdataKhamkkb (dataKhamxtri.filter(item=> item.roomid.toString()===a));        
+    }   
     const setFilData=(items)=>{
         var arrayCC = [];
         var arrayKham = [];
         for (const item of items) {
-            if(item.roomid_kb==464){
+            if(item.roomid==464){
                 arrayCC.push(item);
             }
             else{
-                arrayKham.push(item);
+                if(item.roomid!=666&&item.roomid!=548&&item.roomid!=541){
+                    arrayKham.push(item);
+                }
             }
         }
         setDatacc(arrayCC);    
-        setdataChuyentuyen(arrayKham.filter(item=> item.dm_hinhthucravienid.toString()==='13'));   
-        setdataKham(arrayKham);    
+        setdataChuyentuyen(items.filter(item=> item.dm_hinhthucravienid.toString()==='13'));  
+        setdataKhamxtri(arrayKham.filter(item=> item.ngayrv.toString()!=='01/01 00:00'));    
+        setdataKhamkkb(arrayKham.filter(item=> item.ngayrv.toString()!=='01/01 00:00'));    
+        setdataKham(items);    
     };     
     const onChangeDate = (date, dateString) => {        
         // console.log("date", dateString);
@@ -78,7 +88,7 @@ const BCNgoaitruPage = () => {
                     defaultActiveKey="1"
                     items={[
                     {
-                        label: `BN ngoại CC (${datacc.length})`,
+                        label: `BN Khám Cấp cứu (${datacc.length})`,
                         key: 'bncapcuu',
                         children: [ 
                             <Table   
@@ -92,7 +102,7 @@ const BCNgoaitruPage = () => {
                         ]
                     },
                     {
-                        label: `BN chuyển viện (${dataChuyentuyen.length})`,
+                        label: `BN chuyển tuyến (${dataChuyentuyen.length})`,
                         key: 'nttha22nnt',
                         children: [ 
                             <Table   
@@ -104,9 +114,53 @@ const BCNgoaitruPage = () => {
                             /> ,
                             'Số lượng: '+ dataChuyentuyen.length                     
                         ]
+                    },{
+                        label: `BN Phòng Khám (${dataKhamxtri.length})`,
+                        key: 'tbnngoaitruxtri',
+                        children: [ 
+                            <Select
+                            key={"slkhoa"}
+                            showSearch
+                            style={{
+                                width: '40%',
+                                cursor: 'move',
+                              }}
+                            onChange={changeKhoaKB}
+                            placeholder="Chọn khoa Khám bệnh"
+                            filterOption={(input, option) =>
+                            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                            }
+                            options={[
+                                {value: '434',label: 'Phòng khám số 01'},
+                                {value: '436',label: 'Phòng khám số 07'},
+                                {value: '437',label: 'Phòng khám số 04'},
+                                {value: '438',label: 'Phòng khám số 05'},
+                                {value: '439',label: 'Phòng khám số 06'},
+                                {value: '440',label: 'Phòng khám số 03'},
+                                {value: '441',label: 'Phòng khám số 08'},
+                                {value: '442',label: 'Phòng khám số 09'},                                    
+                                {value: '443',label: 'Phòng khám số 10'},
+                                {value: '444',label: 'Phòng khám số 11'},
+                                {value: '447',label: 'Phòng khám số 13 (PK RHM)'},
+                                {value: '448',label: 'Phòng khám số 12 (PK MẮT)'},
+                                {value: '501',label: 'Phòng Tiêm chủng'},
+                                {value: '509',label: 'Phòng sao bệnh án'},
+                                {value: '594',label: 'Phòng khám Y Học Cổ Truyền'},
+                                {value: '605',label: 'Phòng khám 16'}                                          
+                            ]}
+                        />, 
+                            <Table   
+                            rowKey={"patientrecordid"}                    
+                            bordered
+                            dataSource={dataKhamkkb} columns={columns_nt}                       
+                            key="tbnngoaitruxtritb"
+                            loading={{ indicator: <div><Spin /></div>, spinning:pending}}
+                            /> ,
+                            'Số lượng: '+ dataKhamkkb.length                     
+                        ]
                     },
                     {
-                        label: `BN ngoại trú (${dataKham.length})`,
+                        label: `BN Khám ngoại trú (${dataKham.length})`,
                         key: 'ntthan3nt',
                         children: [ 
                             <Table   
